d2f73d5fa6cc4f92abff7cd8971fab95d7c8229b
Alfred Bisame <alfredbisame@gmail.com>
2026-01-12 11:41:27 +0000

    fix: improved the responsiveness for the update post

--- src/app/components/ManagePost/UpdateProducts.tsx
+++ src/app/components/ManagePost/UpdateProducts.tsx
@@ -225,7 +225,7 @@ const UpdateProducts = () => {
         onCancel={handleCancelEdit}
       />
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5 gap-6 mt-6">
         {updateProducts.map((product) => {
           return (
             <div
